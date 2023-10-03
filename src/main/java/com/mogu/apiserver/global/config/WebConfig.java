5818418652d8139868951005f3b15024116b4af7
prlhspt <prlhspt@gmail.com>
2023-10-03 20:58:23 +0900

    Feat: CORS 구현
    Cross-Origin Resource Sharing (CORS)를 구현하여 웹 애플리케이션에 외부 도메인에서의 요청을 허용하였습니다.
    
    - 적절한 CORS 헤더를 응답에 추가하여 요청 허용
    - 테스트 수행

---
+++ src/main/java/com/mogu/apiserver/global/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.mogu.apiserver.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.PATCH.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+}
